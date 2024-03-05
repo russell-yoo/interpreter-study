1a5d3586f20c70edae89ecad21d15f5a9ff97715
russell yoo <russelly@kakao.com>
2024-03-05 22:54:42 +0900

    token 추가

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifier & literal
+	INDENT = "INDENT"
+	INT    = "INT"
+
+	// operator
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimiter
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keyword
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
